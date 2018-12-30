6e768c4665342df9e5f31ff0915b5a094684c06f
Amy Smith <amicloud.as@gmail.com>
2018-12-29 19:31:13 -0800

    Added all responses

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,22 @@ const Base64 = require('js-base64').Base64;
 
 /* GET library */
 router.get('/library', function (req, res, next) {
-    console.log(req.query);
     let pm = new PlayMusic();
     let username = decodeURIComponent(req.query.username);
     let password = decodeURIComponent(req.query.password);
+    console.log("Attempting to log in");
     pm.login({email: username, password: password}, function (err, info) {
         if (err) {
             console.log(err);
-            console.log(`Invalid username or password. (Or maybe "Allow Less Secure Apps" is off?) Please try again.`);
+            res.json(403, {"error": "Authentication failed."});
+            console.log(`Invalid username or password. (Or maybe "Allow Less Secure Apps" is off)`);
         } else {
             console.log("Logged in successfully!\n" +
                 "Initializing client...");
             pm.init({androidId: info.androidId, masterToken: info.masterToken}, function (err) {
                 if (err) {
                     console.error(err);
+                    res.json(500, {"error": "Failed to create client."});
                     return;
                 }
                 console.log("Client initialized!\n" +
@@ -27,14 +29,15 @@ router.get('/library', function (req, res, next) {
                 pm.getAllTracks({limit: 49990}, function (err, library) {
                     if (err) {
                         console.error(err);
+                        res.json(500, {"error": "Failed to fetch tracks."});
                         return;
                     }
                     console.log("Library retrieved! Found " + library.data.items.length + " songs.");
                     if (library) {
-                        res.json({"library": Base64.encode(json2csv(library.data.items))});
+                        res.json(200, {"library": Base64.encode(json2csv(library.data.items))});
                     } else {
-                        console.log("No songs found. Huh? ¯\\_(ツ)_/¯");
-                        console.log("Are you sure this is the right account?");
+                        res.json(404, {"error": "No songs found."});
+                        console.log("No songs found.");
                     }
                 });
             });
